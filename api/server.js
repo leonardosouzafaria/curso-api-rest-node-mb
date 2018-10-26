2c4bede31f1b1aaf585d49ac5ef079b7aa412ddc
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-10-25 22:06:19 -0300

    Instalanalando o nodemon

--- api/server.js
+++ api/server.js
@@ -1,14 +1,18 @@
-const express = require("express");
+const express = require( "express" );
 const port = 8080;
 //const host = "localhost";
 
 const server = express();
 
-server.get("/", (request, response) => {
+server.get("/", ( request, response ) => {
     response.send("<h1>Home</h1>");
 });
 
-server.listen(port, () => {
+server.get("/foto", ( request, response ) => {
+    response.send("<h1>Foto</h1>");
+});
+
+server.listen( port, () => {
     console.log(`servidor está de na url: http://localhost:${port}`);
     console.log("Para derrubar o servidor  aperte CTRL + C");
-})
+});
